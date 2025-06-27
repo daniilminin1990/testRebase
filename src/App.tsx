108d51809a27eb301ba1c7f6a4d0817fb50f5f99
Daniil Minin <Minin_D@bankrbk.kz>
2025-06-27 19:07:08 +0500

    moved imgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from '../public/folderForAssets/assets/react.svg'
+import viteLogo from '../public/folderForAssets/assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h1>moved images to new dir</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
